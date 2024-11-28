fb46e8a354660a576f042335da398a57ddc68143
BigBoss-101 <kkbdarren@gmail.com>
2024-11-28 20:55:58 +0800

    Karl Update lessors API part

--- app/api/admin/users/lessors/route.ts
+++ app/api/admin/users/lessors/route.ts
@@ -1,13 +1,16 @@
 // app/api/admin/lessors/route.ts
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 
-export async function GET() {
+export async function GET(req: Request) {
   try {
+    const url = new URL(req.url);
+    const searchParams = Object.fromEntries(url.searchParams.entries());
+
     // Fetch users with the role of 'LESSOR' and their subscriptions
     const lessors = await prisma.user.findMany({
       where: {
-        role: 'LESSOR',
+        role: "LESSOR",
       },
       select: {
         id: true,
@@ -20,7 +23,8 @@ export async function GET() {
         idBack: true,
         idType: true,
         image: true,
-        Subscription: { // Include subscription details
+        Subscription: {
+          // Include subscription details
           select: {
             plan: true,
             period: true,
